9078e713e9cdeba2500dc77df0ed0be1c0480896
longeek <wizardof21cn@163.com>
2016-06-26 11:02:11 +0800

    Initial Commit

---
+++ SimpleSsqGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleSsqGenerator
+//
+//  Created by loongeek on 6/26/16.
+//  Copyright © 2016 loongeek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
