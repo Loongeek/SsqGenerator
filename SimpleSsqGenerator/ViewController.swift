a41aeb310e0edd541a1151e21bef3270a06e62df
longeek <wizardof21cn@163.com>
2016-07-02 23:02:47 +0800

    change name; add image resource

--- SimpleSsqGenerator/ViewController.swift
+++ SimpleSsqGenerator/ViewController.swift
@@ -10,6 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    
+    private var resultNumber:[Int]{
+        set {
+            if newValue.count == 7{
+                let numberStr = newValue.reduce("", combine: {(str, number) in
+                    return str + String(number)+" "
+                    })
+                let lastNumberWidth = String(newValue[6]).characters.count+1
+                let numberColoredStr = NSMutableAttributedString(string: numberStr)
+                numberColoredStr.setAttributes(
+                    [NSForegroundColorAttributeName : UIColor.redColor()],
+                    range: NSRange(location: 0, length: numberStr.characters.count))
+                numberColoredStr.setAttributes([NSForegroundColorAttributeName : UIColor.blueColor()], range: NSRange(location: numberStr.characters.count-lastNumberWidth,length: lastNumberWidth))
+                result.attributedText = numberColoredStr
+                
+            }
+            else{
+                result.attributedText = NSAttributedString(string: "个数错误")
+            }
+        }
+        get{
+            return self.resultNumber
+        }
+    }
+    @IBAction func generate(sender: UIButton) {
+//        resultNumber = generateNumber()
+        resultNumber = generateNumberUsingWebAPI()
+    }
+    
+    private func generateNumberUsingWebAPI()->[Int]{
+        let redNumber = generateNumberUsingWebAPI(1,max: 33,count: 6)
+        let blueNumber = generateNumberUsingWebAPI(1,max: 16,count: 1)
+        return redNumber + blueNumber
+    }
+    
+    private func generateNumberUsingWebAPI(min:Int, max:Int, count:Int)->[Int]{
+        //make http request
+        //request
+        //translate returned result
+        return [Int]()
+    }
+    
+    private func generateNumber()->[Int]{
+        var redNumber = Set<Int>()
+        while redNumber.count<6{
+            redNumber.insert(Int(random()%33+1))
+        }
+        let blueNumber = Int(random()%16+1)
+        var result = [Int]()
+        for number in redNumber{
+            result.append(number)
+        }
+        result.append(blueNumber)
+        return result
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
